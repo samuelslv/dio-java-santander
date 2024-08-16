4e91b21d59ff41dee0f504f672bc67218364bb8f
Samuel Silva <samuelslv7@gmail.com>
2024-08-16 18:08:23 -0300

    colocado os TODO's

--- DP1/ContaBanco/src/ContaTerminal.java
+++ DP1/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //TODO: Exibir as mensagens para o usuario
+
+        //TODO: obter pelo scanner os valores digitais no terminal
+
+        //TODO: Exibir a mensagem conta criada'
     }
 }
