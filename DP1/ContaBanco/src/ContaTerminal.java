e1b12b104d91437195b760a1eebaebce52a6cbde
Samuel Silva <samuelslv7@gmail.com>
2024-08-16 18:46:24 -0300

    Finalizado Desafio de projeto 2

--- DP1/ContaBanco/src/ContaTerminal.java
+++ DP1/ContaBanco/src/ContaTerminal.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importar a classe Scanner
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
 
-        //TODO: Exibir as mensagens para o usuario
+        Scanner scanner = new Scanner(System.in);
 
-        //TODO: obter pelo scanner os valores digitais no terminal
+        System.out.println("Olá, qual o seu nome?");
+        nomeCliente = scanner.nextLine();
+        System.out.println("qual o número da sua Agência?");
+        agencia = scanner.nextLine();
+        System.out.println("qual o número da sua Conta?");
+        numero = scanner.nextInt();
+        System.out.println("defina o seu saldo");
+        saldo = scanner.nextDouble();
 
-        //TODO: Exibir a mensagem conta criada'
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        scanner.close();
     }
 }
