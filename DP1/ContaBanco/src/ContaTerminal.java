e05fb3184ce59418111c7ccaec7143df50db7fe4
Samuel Silva <samuelslv7@gmail.com>
2024-08-17 16:53:19 -0300

    modified:   DP1/ContaBanco/src/ContaTerminal.java 	new file:   DP2/DesafioControleFluxo/.vscode/settings.json 	new file:   DP2/DesafioControleFluxo/README.md 	new file:   DP2/DesafioControleFluxo/bin/Contador.class 	new file:   DP2/DesafioControleFluxo/bin/ParametrosInvalidosException.class 	new file:   DP2/DesafioControleFluxo/src/Contador.class 	new file:   DP2/DesafioControleFluxo/src/Contador.java 	new file:   DP2/DesafioControleFluxo/src/ParametrosInvalidosException.class 	new file:   DP2/DesafioControleFluxo/src/ParametrosInvalidosException.java

--- DP1/ContaBanco/src/ContaTerminal.java
+++ DP1/ContaBanco/src/ContaTerminal.java
@@ -24,3 +24,12 @@ public class ContaTerminal {
         scanner.close();
     }
 }
+
+public static void main(String[] args) {
+    double alturaJoao = 1.46;
+    double alturaMarcos = 1.37;
+    if (alturaMarcos > alturaJoao)
+        System.out.println("João é mais alto que Marcos");
+    else
+        System.out.println("Marcos é mais baixo que João");
+}
