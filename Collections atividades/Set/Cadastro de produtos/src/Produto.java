92a9cb50847331caace44a10f0dfd50a66720d3c
Samuel Silva <samuelslv7@gmail.com>
2024-09-17 09:39:34 -0300

    finalizado Cadastro de produtos

---
+++ Collections atividades/Set/Cadastro de produtos/src/Produto.java
@@ -0,0 +1,91 @@
+import java.util.Comparator;
+
+public class Produto implements Comparable<Produto> {
+    private String nome;
+    private long cod;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, long cod, double preco, int quantidade) {
+        this.nome = nome;
+        this.cod = cod;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto [nome=" + nome + ", cod=" + cod + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (cod ^ (cod >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (cod != other.cod)
+            return false;
+        return true;
+    }
+
+}
+
+
+class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+
+    
+}
