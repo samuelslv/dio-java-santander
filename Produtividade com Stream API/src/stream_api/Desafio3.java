7abb1c5eda46ac2ead13e0ee94ea5d841df01ef0
Samuel Silva <samuelslv7@gmail.com>
2024-09-23 21:51:29 -0300

    Desafio 3 finalizado

---
+++ Produtividade com Stream API/src/stream_api/Desafio3.java
@@ -0,0 +1,20 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Desafio 3 - Verifique se todos os números da lista são positivos:
+Com a ajuda da Stream API, verifique se todos os números da lista são positivos e exiba o resultado no console.
+ */
+public class Desafio3 {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean todosPositivos = numeros.stream().allMatch(num -> num >= 0);
+
+        System.out.println("Todos positivos? " + todosPositivos);
+    }
+
+}
