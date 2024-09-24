3f3e4e35bb4a2e422654210a55f6f08a576eeae3
Samuel Silva <samuelslv7@gmail.com>
2024-09-24 12:32:21 -0300

    Desafio 9 finalizado

--- Produtividade com Stream API/src/functional_interface/examples/ConsumerExample.java
+++ Produtividade com Stream API/src/functional_interface/examples/ConsumerExample.java
@@ -8,8 +8,7 @@ import java.util.function.Consumer;
  * Representa uma operação que aceita um argumento do tipo T e não retorna
  * nenhum resultado.
  * É utilizada principalmente para realizar ações, ou efeitos colaterais nos
- * elementos do Stream sem modificar, ou
- * retornar um valor.
+ * elementos do Stream sem modificar, ou retornar um valor.
  */
 public class ConsumerExample {
   public static void main(String[] args) {
